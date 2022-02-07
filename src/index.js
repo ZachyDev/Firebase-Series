2cec3fda33618495413aa49ada65ccd4b3aba960
= <=>
2022-02-08 01:28:52 +0300

    Firebase setup and fetching data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDgFT_a4AFYV17_yeMlwQOEZZGJ71Rw2Gk",
@@ -8,3 +9,21 @@ const firebaseConfig = {
     messagingSenderId: "926747255617",
     appId: "1:926747255617:web:8870b670471dd9fa26b7c0"
   };
+
+ initializeApp(firebaseConfig);
+
+//  initialize services
+const db = getFirestore();
+
+// collection reference
+const colRef = collection(db, 'students');
+
+getDocs(colRef)
+  .then(snapshot => {
+    let students = [];
+    snapshot.forEach(doc => {
+      students.push({ ...doc.data(), id:doc.id })
+    })
+    console.log(students);
+  })
+  .catch(err => console.log(err.message));
